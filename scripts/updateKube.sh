68197470f577d9bf0cc8f885db2c8450bc3c84cb
Oladotunn <stevedot.olabode@gmail.com>
2021-06-08 12:18:25 +0100

    k8s path updated

--- scripts/updateKube.sh
+++ scripts/updateKube.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" k8s/deployment.yml
-cat k8s/deployment.yml
+sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" k8s/deployment.yaml
+cat k8s/deployment.yaml
