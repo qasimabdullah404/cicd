16949c5dc5b994286736374b85577f94150d94c4
Oladotunn <stevedot.olabode@gmail.com>
2021-06-16 15:23:02 +0100

    deploy updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-# cat ${HOME}/.kube/config | base64 | pbcopy
-# echo "$KUBE_CONFIG" | base64 --decode > ${HOME}/.kube/config
-echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
-/usr/local/bin/kubectl \
-  --kubeconfig=/dev/null \
-  --server=$KUBERNETES_SERVER \
-  --certificate-authority=cert.crt \
-  --token=$KUBERNETES_TOKEN \
-  apply -f ./k8s/
-echo "The build number is ${TRAVIS_BUILD_NUMBER}"
-/usr/local/bin/kubectl \
-  --kubeconfig=/dev/null \
-  --server=$KUBERNETES_SERVER \
-  --certificate-authority=cert.crt \
-  --token=$KUBERNETES_TOKEN \
-  set image deployment/travispy travispy=dotunn/travis:py01 --record
+docker run -v ${TRAVIS_BUILD_DIR}:/kube smesch/kubectl kubectl --kubeconfig /kube/kubeconfig set image deployment/travispy travispy=dotunn/travis:py01
+
+
+
+
+
+# /usr/local/bin/kubectl \
+#  --kubeconfig=/dev/null \
+#  --server=$KUBERNETES_SERVER \
+#  --certificate-authority=cert.crt \
+#  --token=$KUBERNETES_TOKEN \
+#  apply -f ./k8s/
+#echo "The build number is ${TRAVIS_BUILD_NUMBER}"
+#/usr/local/bin/kubectl \
+#  --kubeconfig=/dev/null \
+#  --server=$KUBERNETES_SERVER \
+#  --certificate-authority=cert.crt \
+#  --token=$KUBERNETES_TOKEN \
+#  set image deployment/travispy travispy=dotunn/travis:py01 --record
