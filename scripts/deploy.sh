4cab2cf1e361dc4cc10cca70809dfe4be29ca59a
Oladotunn <stevedot.olabode@gmail.com>
2021-06-18 11:34:59 +0100

    k8s deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+git add . && git commit -m "deploy script added" && git status
 docker run -v ${TRAVIS_BUILD_DIR}:/kube smesch/kubectl kubectl --kubeconfig kubeconfig set image deployment/travispy travispy=dotunn/travis:node0
